17157cd5bd1c4286b9942c3d71f7e0150c1cc07f
bill chipman <wrchipman@gmail.com>
2023-10-02 22:27:37 -0400

    Ready for new buttons

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.cis386_first_android_app"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.cis386_first_android_app"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
